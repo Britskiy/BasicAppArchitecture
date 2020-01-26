cce113485a5104a6ca1c18539b5a1c591350e212
Сергей Брицкий <warriormagistr@gmail.com>
2020-01-26 18:23:36 +0200

    cleanup

--- test/data/user.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userData = {
-    login: "test1",
-    password: "password",
-    firstName: "test1",
-    lastName: "test1",
-    email: "test1@test.com"
-};
