49cf9b57513d23ba229dcdbe436ba0ad2b48a725
Сергей Брицкий <warriormagistr@gmail.com>
2020-03-01 17:40:45 +0200

    changes into gulp for run tests

--- gulpfile.js
+++ gulpfile.js
@@ -12,19 +12,25 @@ var gutil = require('gulp-util');
 var spawn = require('child_process').spawn,
     node;
 
-let config = {
-    "compilerOptions": {
-        "module": "commonjs",
-        "target": "es6",
-        "outDir": "out",
-        "lib": [ "es5","es2015", "es6",
-        "dom"],
-        "sourceMap": true,
-        "rootDir": ".",
-        "allowJs": true,
-        "types": [ "node" ]
-    }
-}
+// let config = {
+//     "compilerOptions": {
+//         "module": "commonjs",
+//         "target": "es6",
+//         "outDir": "out",
+//         "lib": [ "es5","es2015", "es6",
+//         "dom"],
+//         "sourceMap": true,
+//         "rootDir": ".",
+//         "allowJs": true,
+//         "typeRoots": [
+//             "node_modules/@types"
+//         ],
+//         "types": [
+//             "node",
+//             "mocha"
+//         ]
+//     }
+// }
 
 function handleError(err) {
     console.log(err.toString());
@@ -41,47 +47,42 @@ gulp.task('clean_src', function() {
 });
 
 gulp.task('compile-tests-data', function() {
-    return tsProject.src()
-        .pipe(typescript(config.compilerOptions))
-        .pipe(gulp.dest('.'))
+    return gulp.src('test/**/*.ts')
+        .pipe(tsProject())
+        .pipe(gulp.dest('./test/'))
 })
 
 //Mocha tests
 gulp.task('tests', function() {
-    return tsProject
-        .src()
-        .pipe(typescript(config.compilerOptions))
-        .pipe(gulp.dest('dist')).on('end', function() {
+    
+    del('./test/*.js');
 
-        del('./test/*.js');
-
-        tsProject.src()
-            .pipe(typescript(config.compilerOptions))
-            .pipe(gulp.dest('./test/'))
-            .pipe(mocha({
-                debugBrk: DEBUG,
-                exit: true,
-                env: { 'NODE_ENV': 'test' },
-                reporter: 'spec'
-        })).on('error', handleError).on('end', function() {
-            // у нас все закончилось успешно
-        }) 
-    });
+    return gulp.src('./test/**/*.ts')
+        .pipe(typescript())
+        .pipe(gulp.dest('./test/'))
+        .pipe(mocha({
+            debugBrk: DEBUG,
+            exit: true,
+            env: { 'NODE_ENV': 'test' },
+            reporter: 'spec'
+    })).on('error', handleError).on('end', function() {
+        // у нас все закончилось успешно
+    }) 
 });
 
 // TypeScript compile
 gulp.task('compile', function() {
-    return tsProject.src()
+    return gulp.src('src/**/*.ts')
     .pipe(tsProject())
-    .js.pipe(gulp.dest('dist'));
+    .js.pipe(gulp.dest('./dist'));
 });
 
 // ADD for fix Mocha path
 gulp.task('dist', gulp.series('clean', 'clean_src'), function() {
-    return tsProject.src().pipe(typescript(config.compilerOptions)).pipe(gulp.dest('dist'));
+    return gulp.src('src/**/*.ts').pipe(tsProject()).pipe(gulp.dest('./dist'));
 });
 
-gulp.task('test', gulp.series('compile-tests-data', 'tests'));
-gulp.task('build', gulp.series('compile'));
+gulp.task('build', gulp.series('clean_src','clean','compile'));
+gulp.task('test', gulp.series('build', 'compile-tests-data', 'tests'));
 gulp.task('default', gulp.series('build'));
 //gulp.task('compile', ['handlebars']);
