dbd255dcb549b6f83c550486c8c0d6fa0923b0f1
Sergey Brickiy <warriormagistr@gmail.com>
2020-11-25 00:15:51 +0200

    fix base types

--- src/interfaces/base.ts
+++ src/interfaces/base.ts
@@ -2,21 +2,23 @@ import mongoose = require("mongoose");
 
 export interface IRead<T extends mongoose.Document> {
     retrieve: (callback: (error: any, result: any) => void) => void;
-    findById: (id: string, callback: (error: any, result: T) => void) => void;
-    findOne(conditions?: any, callback?: (err: any, res: T | null) => void): mongoose.DocumentQuery<T | null, T>;
-    findOne(conditions: any, projection: any, callback?: (err: any, res: T | null) => void): mongoose.DocumentQuery<T | null, T>;
-    findOne(conditions: any, projection: any, options: any, callback?: (err: any, res: T | null) => void): mongoose.DocumentQuery<T | null, T>;
+    findById: (id: string, callback: (error: any, result: mongoose.Model<T>) => void) => void;
+    findOne(conditions?: any, callback?: (err: any, res: mongoose.Model<T> | null) => void): mongoose.DocumentQuery<T | null, T>;
+    findOne(conditions: any, projection: any, callback?: (err: any, res: mongoose.Model<T> | null) => void): mongoose.DocumentQuery<T | null, T>;
+    findOne(conditions: any, projection: any, options: any, callback?: (err: any, res: mongoose.Model<T> | null) => void): mongoose.DocumentQuery<T | null, T>;
   
-    find(callback?: (err: any, res: T[]) => void): mongoose.DocumentQuery<T[], T>;
-    find(conditions: any, callback?: (err: any, res: T[]) => void): mongoose.DocumentQuery<T[], T>;
+    find(callback?: (err: any, res: mongoose.Model<T>[]) => void): mongoose.DocumentQuery<T[], T>;
+    find(conditions: any, callback?: (err: any, res: mongoose.Model<T>[]) => void): mongoose.DocumentQuery<T[], T>;
+    
     find(conditions: any, projection?: any | null,
       callback?: (err: any, res: T[]) => void): mongoose.DocumentQuery<T[], T>;
+
     find(conditions: any, projection?: any | null, options?: any | null,
-      callback?: (err: any, res: T[]) => void): mongoose.DocumentQuery<T[], T>;
+      callback?: (err: any, res: mongoose.Model<T>[]) => void): mongoose.DocumentQuery<T[], T>;
 }
 
 export interface IWrite<T extends mongoose.Document> {
-    create: (item: T, callback: (error: any, result: any) => void) => void;
-    update: (_id: mongoose.Types.ObjectId, item: T, callback: (error: any, result: any) => void) => void;
+    create: (item: mongoose.CreateQuery<T>, callback: (error: any, result: T[]) => void) => void;
+    update: (_id: mongoose.Types.ObjectId, item: mongoose.UpdateQuery<T>, callback: (error: any, result: any) => void) => void;
     delete: (_id: string, callback: (error: any, result: any) => void) => void;
 }
--- src/repositories/base.ts
+++ src/repositories/base.ts
@@ -9,7 +9,7 @@ export class RepositoryBase<T extends mongoose.Document> implements IRead<T>, IW
         this._model = schemaModel;
     }
 
-    create(item: T, callback: (error: any, result: T) => void) {
+    create(item: mongoose.CreateQuery<T>, callback?: (err: any, res: T[]) => void)  {
         this._model.create(item, callback);
     }
 
@@ -17,24 +17,24 @@ export class RepositoryBase<T extends mongoose.Document> implements IRead<T>, IW
         this._model.find({}, callback);
     }
 
-    update(_id: mongoose.Types.ObjectId, item: T, callback: (error: any, result: any) => void) {
-        this._model.update({ _id: _id }, item, callback);
+    update(_id: mongoose.Types.ObjectId, item: mongoose.UpdateQuery<T>, callback: (error: any, result: any) => void) {
+        this._model.update({ _id: _id } as mongoose.FilterQuery<T>, item, callback);
     }
 
     delete(_id: string, callback: (error: any, result: any) => void) {
-        this._model.deleteOne({ _id: this.toObjectId(_id) }, (err) => callback(err, null));
+        this._model.deleteOne({ _id: this.toObjectId(_id) } as mongoose.FilterQuery<T>, (err) => callback(err, null));
     }
 
-    findById(_id: string, callback: (error: any, result: T) => void) {
+    findById(_id: string, callback: (error: any, result: mongoose.Model<T>) => void) {
         this._model.findById(_id, callback);
     }
 
-    findOne(cond?: Object, callback?: (err: any, res: T) => void): mongoose.DocumentQuery<T | null, T> {
-        return this._model.findOne(cond, callback);
+    findOne(cond?: Object, callback?: (err: any, res: mongoose.Model<T>) => void): mongoose.DocumentQuery<T | null, T> {
+        return this._model.findOne(cond as mongoose.FilterQuery<T>, callback);
     }
 
     find(cond?: any, options?: any, callback?: (err: any, res: T[]) => void): mongoose.DocumentQuery<T[],  T> {
-        return this._model.find(cond, options, callback);
+        return this._model.find(cond as mongoose.FilterQuery<T>, options, callback);
     }
 
     private toObjectId(_id: string): mongoose.Types.ObjectId {
